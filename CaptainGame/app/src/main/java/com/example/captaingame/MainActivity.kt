0ea7062ae6f680a1d298dd2dd4145bd5cf7afbae
joonda <kloudzero@kakao.com>
2026-01-08 22:04:57 +0900

    change directory

---
+++ CaptainGame/app/src/main/java/com/example/captaingame/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.captaingame
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.example.captaingame.ui.theme.CaptainGameTheme
+import kotlin.random.Random
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CaptainGameTheme {
+                Scaffold (
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    CaptainGame(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun CaptainGame(modifier: Modifier) {
+        val treasuresFound = remember { mutableStateOf(0) }
+        //val treasuresFound by remember { mutableStateOf(0) }
+        val direction = remember { mutableStateOf("North") }
+        val stormOrTreasure = remember { mutableStateOf("") }
+
+        Column (modifier.fillMaxSize()) {
+            Text(text = "Treasures Found: ${treasuresFound}")
+            Text(text = "Current Direction: ${direction.value}")
+            Text(text = stormOrTreasure.value)
+            Button(onClick = {
+                direction.value = "East"
+                if (Random.nextBoolean()) {
+                    treasuresFound.value += 1
+                    stormOrTreasure.value = "WE FOUND A TREASURE!!!"
+                } else {
+                    stormOrTreasure.value = "STORM AHEAD!!!"
+                }
+            }) {
+                Text("Sail East")
+            }
+
+            Button(onClick = {
+                direction.value = "West"
+                if (Random.nextBoolean()) {
+                    treasuresFound.value += 1
+                    stormOrTreasure.value = "WE FOUND A TREASURE!!!"
+                } else {
+                    stormOrTreasure.value = "STORM AHEAD!!!"
+                }
+            }) {
+                Text("Sail West")
+            }
+
+            Button(onClick = {
+                direction.value = "North"
+                if (Random.nextBoolean()) {
+                    treasuresFound.value += 1
+                    stormOrTreasure.value = "WE FOUND A TREASURE!!!"
+                } else {
+                    stormOrTreasure.value = "STORM AHEAD!!!"
+                }
+            }) {
+                Text("Sail North")
+            }
+
+            Button(onClick = {
+                direction.value = "South"
+                if (Random.nextBoolean()) {
+                    treasuresFound.value += 1
+                    stormOrTreasure.value = "WE FOUND A TREASURE!!!"
+                } else {
+                    stormOrTreasure.value = "STORM AHEAD!!!"
+                }
+            }) {
+                Text("Sail South")
+            }
+        }
+    }
+}
