59fd2fd21399013622c4ec16bae8d255a5c49b94
joonda <kloudzero@kakao.com>
2026-01-18 20:49:51 +0900

    remove unuse import

--- MyShoppingListApp/app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
+++ MyShoppingListApp/app/src/main/java/com/example/myshoppinglistapp/ShoppingList.kt
@@ -18,9 +18,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
