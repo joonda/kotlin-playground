d6155287ee4840e80c79dc0d7fd827cd17fc1891
joonda <kloudzero@kakao.com>
2026-01-26 21:00:29 +0900

    practice

--- MyRecipeApp/app/src/main/java/com/example/myrecipeapp/MainActivity.kt
+++ MyRecipeApp/app/src/main/java/com/example/myrecipeapp/MainActivity.kt
@@ -2,22 +2,30 @@ package com.example.myrecipeapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.example.myrecipeapp.ui.theme.MyRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+            val isDark = isSystemInDarkTheme()
+            enableEdgeToEdge(
+                statusBarStyle = if(isDark) {
+                    SystemBarStyle.dark(Color.Black.toArgb())
+                } else {
+                    SystemBarStyle.light(scrim = Color.White.toArgb(), darkScrim = Color.White.toArgb())
+                }
+            )
             MyRecipeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     RecipeScreen(modifier = Modifier.padding(innerPadding))
