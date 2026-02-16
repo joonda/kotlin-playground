a604a8ddbfd7e72cea638335b49e48b2dfb9b611
joonda <kloudzero@kakao.com>
2026-02-16 16:15:29 +0900

    add dummy data, app view

--- MyWishListApp/app/src/main/java/com/example/mywishlistapp/HomeView.kt
+++ MyWishListApp/app/src/main/java/com/example/mywishlistapp/HomeView.kt
@@ -1,23 +1,37 @@
 package com.example.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.mywishlistapp.data.DummyWish
+import com.example.mywishlistapp.data.Wish
 
 @Composable
-fun HomeView(modifier: Modifier) {
+fun HomeView(
+    navController: NavController,
+    viewModel: WishViewModel
+) {
     val context = LocalContext.current
 
     Scaffold(
@@ -35,13 +49,39 @@ fun HomeView(modifier: Modifier) {
                 shape = CircleShape,
                 onClick = {
                     Toast.makeText(context, "Button Clicked", Toast.LENGTH_SHORT).show()
+                    navController.navigate(Screen.AddScreen.route)
                 }) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
             }
         }
     ) {
-        LazyColumn(modifier = modifier
+        LazyColumn(modifier = Modifier
             .fillMaxSize()
-            .padding(it)) { }
+            .padding(it)) {
+            items(DummyWish.wishList) {
+                wish -> WishItem(wish = wish) { }
+
+            }
+        }
+    }
+}
+
+@Composable
+fun WishItem(wish: Wish, onClick: () -> Unit) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+        .clickable {
+            onClick()
+        },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        colors = CardDefaults.cardColors(Color.White)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = wish.title, fontWeight = FontWeight.ExtraBold)
+            Text(text = wish.description)
+        }
     }
 }
--- MyWishListApp/app/src/main/java/com/example/mywishlistapp/MainActivity.kt
+++ MyWishListApp/app/src/main/java/com/example/mywishlistapp/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.example.mywishlistapp.ui.theme.MyWishListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,9 +12,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyWishListAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeView(modifier = Modifier.padding(innerPadding))
-                }
+                Navigation()
             }
         }
     }
